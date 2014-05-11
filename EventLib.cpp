561b95d7052580669e00811b6760b8c22ae5d03a
FCO <fernandocorrea@gmail.com>
2014-05-10 23:52:42 -0300

    Creating recurrent events

--- EventLib.cpp
+++ EventLib.cpp
@@ -9,11 +9,40 @@ EventLib::EventLib(){
 void EventLib::add_listener(void *event_data,
                   int (*test_func)(void *),
                   void (*handle_func)(void *)) {
+  struct _event_listener *new_listener = create_event_listener(event_data, test_func, handle_func);
+  add_listener_to_list(new_listener);
+}
+
+void EventLib::add_listener(void *event_data,
+                  int (*test_func)(void *),
+                  void (*handle_func)(void *),
+                  int flags) {
+  struct _event_listener *new_listener = create_event_listener(event_data, test_func, handle_func, flags);
+  add_listener_to_list(new_listener);
+}
+
+struct EventLib::_event_listener * EventLib::create_event_listener(
+                  void *event_data,
+                  int (*test_func)(void *),
+                  void (*handle_func)(void *)) {
+  struct _event_listener *new_listener = (event_listener *) malloc(sizeof(event_listener));
+  new_listener->event_data = event_data;
+  new_listener->test_func = test_func;
+  new_listener->handle_func = handle_func;
+  new_listener->recurrent = 0;
+  new_listener->count = 0;
+}
+
+struct EventLib::_event_listener * EventLib::create_event_listener(void *event_data,
+                  int (*test_func)(void *),
+                  void (*handle_func)(void *),
+                  int flags) {
+  struct _event_listener *new_listener = create_event_listener(event_data, test_func, handle_func);
+  if(flags & RECURRENT_EVENT) new_listener->recurrent = 1;
+}
+
+void EventLib::add_listener_to_list(struct _event_listener *new_event) {
   struct _event_node *new_node = (struct _event_node *) malloc(sizeof(struct _event_node));
-  new_node->event = (event_listener *) malloc(sizeof(event_listener));
-  new_node->event->event_data = event_data;
-  new_node->event->test_func = test_func;
-  new_node->event->handle_func = handle_func;
   if(!first_event) {
     first_event = new_node;
   } else {
@@ -23,17 +52,35 @@ void EventLib::add_listener(void *event_data,
   last_event = new_node;
 }
 
-void EventLib::check_and_handle_event(struct _event_listener *listener) {
+void EventLib::remove_listener_from_list(struct _event_node *node) {
+  if(node == first_event)
+    first_event = node->next;
+  if(node == last_event)
+    last_event = node->prev;
+
+  if(node->prev)
+    node->prev->next = node->next;
+  if(node->next)
+    node->next->prev = node->prev;
+  free(node);
+}
+
+void EventLib::check_and_handle_event(struct _event_node *node) {
+  struct _event_listener *listener = node->event;
   void *event_data = listener->event_data;
   if(listener->test_func(event_data)) {
+    remove_listener_from_list(node);
     listener->handle_func(event_data);
+    listener->count++;
+    if(listener->recurrent)
+      add_listener_to_list(listener);
   }
 }
 
 void EventLib::event_loop() {
   struct _event_node *curr = first_event;
   while(curr) {
-    check_and_handle_event(curr->event);
+    check_and_handle_event(curr);
     curr = curr->next;
   }
 }
--- EventLib.h
+++ EventLib.h
@@ -3,11 +3,14 @@
 #include "Arduino.h"
 
 class EventLib {
+	#define RECURRENT_EVENT 0b0001
 
 	typedef struct _event_listener{
 		void *event_data;
 		int (*test_func)(void *);
 		void (*handle_func)(void *);
+		int recurrent;
+		int count;
 	} event_listener;
 
 	typedef struct _event_node{
@@ -18,14 +21,19 @@ class EventLib {
 
 	public:
 		EventLib();
-		void add_listener(void *event_data, int (*test_func)(void *), void (*handle_func)(void *));
-		void check_and_handle_event(struct _event_listener *listener);
+		void add_listener(void *, int (*)(void *), void (*)(void *));
+		void add_listener(void *, int (*)(void *), void (*)(void *), int);
 		void set_timeout_to_run(unsigned int, void(*)(void *));
 		void event_loop();
 	private:
 		event_listener *listeners[10];
 		struct _event_node *first_event;
 		struct _event_node *last_event;
+		void check_and_handle_event(struct _event_node *);
+		void add_listener_to_list(struct _event_listener *);
+		struct _event_listener * create_event_listener(void *, int (*)(void *), void (*)(void *));
+		struct _event_listener * create_event_listener(void *, int (*)(void *), void (*)(void *), int);
+		void remove_listener_from_list(struct _event_node *);
 };
 
 #endif
